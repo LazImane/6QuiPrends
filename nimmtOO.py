68cc91e88a21c10409c04639fccea213270a1eb3
AnecV8 <164578989+AnecV8@users.noreply.github.com>
2024-04-22 16:09:08 +0200

    nimmtOO.py

--- nimmtOO.py
+++ nimmtOO.py
@@ -5,12 +5,31 @@ def interactiveRun():
     print("Bienvenue sur le jeu 6 qui prend !")
     while True:
         try:
-            
             num_players = int(input("Combien de joueurs ? "))
             players=[]
             for i in range(num_players):
-                name=input("Nom du joueur : ")
-                players.append(HumanPlayer(name))
+                player_type = input(f"Type du joueur {i+1} (humain ou bot) : ").lower()
+                if player_type == 'humain':
+                    name = input("Nom du joueur : ")
+                    players.append(HumanPlayer(name))
+                elif player_type == 'bot':
+                    print("Veuillez choisir parmi les bots suivants : goat, moat, poat, zut, roat, loo")
+                    bot_choice = input("Nom du bot : ").lower()
+                    if bot_choice == 'goat':
+                        players.append(None)  #remplace None with goat class
+                    elif bot_choice == 'moat':
+                        players.append(None)   #remplace None with poat class
+                    elif bot_choice == 'poat':
+                        players.append(None)   #remplace None with poat class
+                    elif bot_choice == 'zut':
+                        players.append(None)   #remplace None with zut class
+                    elif bot_choice == 'roat':
+                        players.append(None)   #remplace None with roat class
+                    elif bot_choice == 'loo':
+                        players.append(None)    #remplace None with loo class
+                else:
+                    print("Type de joueur invalide. Veuillez entrer 'humain' ou 'bot'.")
+                    return
             game=NimmtGame(players)
             scores, winners=game.play()
 
