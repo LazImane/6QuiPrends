a50feafb020f64a5ba206a6a896d28c40a1f869e
LazImane <164360620+LazImane@users.noreply.github.com>
2024-04-22 16:09:27 +0200

    Update TheGoat.py

--- player/TheGoat.py
+++ player/TheGoat.py
@@ -1,6 +1,6 @@
 from players.player import Player
 from game.card import Card
-#from game.nimmtGame import NimmtGame  
+import random
 
 class TheGOAT(Player):
     def __init__(self):
@@ -19,43 +19,35 @@ class TheGOAT(Player):
         :param game: le jeu en cours
         :return: la ligne à enlever
         """
-        score = 0
         ligne = 1
         for i,row in enumerate(game.table):
-            if(score > game.total_cows(row)):
+            score = game.total_cows(row)
+            if(score < game.total_cows(row)):
                 ligne = i
+        return ligne
 
         
     def getCardToPlay(self):    
         """
-        Permet d'obtenir la carte à jouer.
+        Choisie aléatoirement une carte.
 
-        :return: La réponse du joueur.
-        """    
-        while True:
-            try:
-                response = int(input(f"@{self.name} ({self.score}🐮) quelle carte voulez-vous jouer ? "))
-                if response <= 0:
-                    raise ValueError
-                return response
-            except ValueError:
-                self.info("Veuillez entrer un nombre entier positif.")
+        :return: une carte aléatoire selon un index random entre 0 et la taille du tableau hand
+        """ 
+        i =  random.randint(0,len(self.hand))
+        return self.hand[i]
+
+
+
     
     def player_turn(self, game):
         """
         Gère le tour de jeu d'un joueur.
-
         :param game : le jeu en cours
         """
-        self.info(game.display_scores())
-        self.info(game.display_table())
         while True:
-            self.info(f"Votre main : {' '.join(map(str, self.hand))}")
             try:
                 carteChoisie = Card(self.getCardToPlay())
                 if carteChoisie in self.hand:
                     return carteChoisie
-                else:
-                    self.info("Vous n'avez pas cette carte dans votre main")
             except ValueError:
                 self.info("Veuillez entrer un nombre entier correspondant à une carte dans votre main.")
