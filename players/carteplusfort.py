e268f468704a646a230d7aa9e7b2eb4746c81c12
miiiiiiimiii <167780958+miiiiiiimiii@users.noreply.github.com>
2024-04-26 11:55:43 +0200

    theStrong.py

--- players/carteplusfort.py
+++ players/carteplusfort.py
@@ -1,16 +1,17 @@
 from players.player import Player 
 from game.card import Card 
+ 
 
 class TheStrong(Player):
-    def __init__(self) -> None:
+    def __init__(self):
        
         self.name='The Strong'
         self.score=0
         self.hand=[]
 
     def info(self, message):
-        
-        print("@"+self.name+" : ",message)
+         print(f"@{self.name} : {message}")
+  
 
     def getCardToPlay(self):
         if not self.hand: 
@@ -21,9 +22,12 @@ class TheStrong(Player):
     
     def getLineToRemove(self, game):
        
-         if not game.table:  # Vérifie si la table de jeu est vide
-            return None
-         return game.table.index(max(game.table)) 
+         score = game.total_cows(game.table[0])
+         for i, row in enumerate(game.table):
+            if score >= game.total_cows(row):
+                score = game.total_cows(row)
+                ligne = i
+         return ligne
 
     def player_turn(self, game):
         # Gère le tour de jeu du bot
@@ -38,3 +42,5 @@ class TheStrong(Player):
         self.info(f"Le bot joue la carte : {card_to_play}")
 
         return card_to_play
+
+        
