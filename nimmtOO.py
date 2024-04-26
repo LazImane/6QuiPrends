a218a8b6d6cead7d56f52ac5a135b2387bafa4da
AnecV8 <164578989+AnecV8@users.noreply.github.com>
2024-04-26 12:16:51 +0200

    Update nimmtOO.py

--- nimmtOO.py
+++ nimmtOO.py
@@ -1,47 +1,74 @@
+
 from players.humanPlayer import HumanPlayer
-from game.nimmtGame import NimmtGame     
+from game.nimmtGame import NimmtGame  
+from players.medianCardBot import MedianCardBot 
+from players.theWeak import TheWeak  
+from players.echantillionage import Echantillions
+from players.minmax import minmaxPlayer 
+from players.carteplusfort import CartePlusForte
+from players.random import Random
+import matplotlib.pyplot as plt
 
-def interactiveRun():
-    print("Bienvenue sur le jeu 6 qui prend !")
-    while True:
-        try:
-            num_players = int(input("Combien de joueurs ? "))
-            players=[]
-            for i in range(num_players):
-                player_type = input(f"Type du joueur {i+1} (humain ou bot) : ").lower()
-                if player_type == 'humain':
-                    name = input("Nom du joueur : ")
-                    players.append(HumanPlayer(name))
-                elif player_type == 'bot':
-                    print("Veuillez choisir parmi les bots suivants : goat, moat, poat, zut, roat, loo")
-                    bot_choice = input("Nom du bot : ").lower()
-                    if bot_choice == 'goat':
-                        players.append(None)  #remplace None with goat class
-                    elif bot_choice == 'moat':
-                        players.append(None)   #remplace None with poat class
-                    elif bot_choice == 'poat':
-                        players.append(None)   #remplace None with poat class
-                    elif bot_choice == 'zut':
-                        players.append(None)   #remplace None with zut class
-                    elif bot_choice == 'roat':
-                        players.append(None)   #remplace None with roat class
-                    elif bot_choice == 'loo':
-                        players.append(None)    #remplace None with loo class
-                else:
-                    print("Type de joueur invalide. Veuillez entrer 'humain' ou 'bot'.")
-                    return
-            game=NimmtGame(players)
-            scores, winners=game.play()
+def playMultipleGames(num_games):
+    # Initialiser un dictionnaire pour stocker le nombre de victoires par bot
+    victoires_par_strategie = {
+        "Random": 0,
+        "TheWeak": 0,
+        "MedianCardBot": 0,
+        "Echantillions": 0,
+        "minmaxPlayer":0,
+        "CartePlusForte":0
+    }
+    num_players = int(input("Combien de joueurs?"))
+    for _ in range(num_games):
+        players = [
+            Random(),
+            TheWeak(),
+            MedianCardBot(),
+            Echantillions(num_players),
+            minmaxPlayer(),
+            CartePlusForte()
+        ]
+        game = NimmtGame(players)
+        scores, winners = game.play()
+        
+        # Mettre à jour le nombre de victoires pour chaque stratégie
+        for player in winners:
+            if isinstance(player, Random):
+                victoires_par_strategie["Random"] += 1
+            elif isinstance(player, TheWeak):
+                victoires_par_strategie["TheWeak"] += 1
+            elif isinstance(player, MedianCardBot):
+                victoires_par_strategie["MedianCardBot"] += 1
+            elif isinstance(player, Echantillions):
+                victoires_par_strategie["Echantillions"] += 1
+            elif isinstance(player,minmaxPlayer):
+                victoires_par_strategie["minmaxPlayer"] += 1
+            elif isinstance(player,CartePlusForte):
+                victoires_par_strategie["CartePlusForte"] += 1
 
-            print("La partie est terminée!")
-            print("Scores finaux :")
-            for playername, score in scores.items(): 
-                print(f"Joueur {playername} : {score} points")
-            s=" ".join([player.name for player in winners])
-            print("Vainqueurs(s) : ",s," !")
-            break
-        except ValueError:
-            print("Veuillez entrer un nombre entier.")
+    print("Nombre de victoires par stratégie :")
+    for strategie, victoires in victoires_par_strategie.items():
+        print(f"{strategie}: {victoires} victoire(s)")
+    
+
+    # the graph
+    strategies = list(victoires_par_strategie.keys())
+    victoires = list(victoires_par_strategie.values())
+
+    plt.bar(strategies, victoires, color=['#4287f5', '#ff9c00', '#23c48e', '#d33682'])
+    plt.xlabel("Stratégies", fontsize=12, fontweight='bold')
+    plt.ylabel("Nombre de victoires", fontsize=12, fontweight='bold')
+    plt.title("Nombre de victoires gagnées par bot", fontsize=14, fontweight='bold')
+
+    # Personnaliser les étiquettes des axes x et y
+    plt.xticks(fontsize=8, rotation=90) 
+
+    plt.tight_layout() 
+
+    plt.show()
 
 if __name__ == "__main__":
-    interactiveRun()
+    num_games = int(input("Combien de parties souhaitez-vous jouer ? "))
+    playMultipleGames(num_games)
+
