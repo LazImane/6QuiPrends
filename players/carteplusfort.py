f03da0542cb23254a05718b849afb9cf0f6cbaf4
LazImane <164360620+LazImane@users.noreply.github.com>
2024-04-26 12:25:00 +0200

    Update carteplusfort.py

--- players/carteplusfort.py
+++ players/carteplusfort.py
@@ -1,46 +1,45 @@
 from players.player import Player 
 from game.card import Card 
- 
 
 class TheStrong(Player):
-    def __init__(self):
-       
+    def __init__(self) -> None:
         self.name='The Strong'
         self.score=0
         self.hand=[]
 
-    def info(self, message):
-         print(f"@{self.name} : {message}")
-  
+    def info(self):
+        pass
+
+    def carteMax(self):
+        carteMax = self.hand[0]
+        for card in self.hand :
+            if card.value >= carteMax.value :
+                carteMax = card
+        return carteMax
 
     def getCardToPlay(self):
         if not self.hand: 
             return None
-        return max(self.hand)
+        return self.carteMax()
 
    
     
     def getLineToRemove(self, game):
-       
-         score = game.total_cows(game.table[0])
-         for i, row in enumerate(game.table):
-            if score >= game.total_cows(row):
+        score = game.total_cows(game.table[0])
+        for i,row in enumerate(game.table):
+            #print("lihne :",i," score ",score)
+            if(score >= game.total_cows(row)):
                 score = game.total_cows(row)
                 ligne = i
-         return ligne
+        return ligne
 
     def player_turn(self, game):
-        # Gère le tour de jeu du bot
-        self.info(game.display_scores())
-        self.info(game.display_table())
 
         if not self.hand:  # Vérifie si la main du bot est vide
-            self.info("La main du bot est vide.")
+            #self.info("La main du bot est vide.")
             return None
 
         card_to_play = self.getCardToPlay()
-        self.info(f"Le bot joue la carte : {card_to_play}")
+        #self.info(f"Le bot joue la carte : {card_to_play}")
 
         return card_to_play
-
-        
